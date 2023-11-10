05eb1d9b319a88224f0feea60a17b16d6db3f3d1
aguilarkevin <kevinaguilar0813@gmail.com>
2023-11-09 22:02:31 -0600

    wrap app with providers component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-2">
+      <h1 className="text-6xl font-bold">Hello World</h1>
+    </main>
+  )
 }

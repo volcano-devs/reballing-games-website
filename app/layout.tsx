4d094ec74e1f651b6bb58af6aa81d436c22ea8b3
aguilarkevin <kevinaguilar0813@gmail.com>
2023-11-10 12:02:47 -0600

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import {Inter} from 'next/font/google'
 import './globals.css'
 import {Providers} from './providers'
 
-import PageLayout from '@components/PageLayout'
-
 const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
@@ -13,11 +11,9 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-  const isMenuOpen = false
-
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} red-dark`}>
         <Providers>{children}</Providers>
       </body>
     </html>

9b952ed34dbcf63b37b48e80b8f32f0d1c014bec
aguilarkevin <kevinaguilar0813@gmail.com>
2023-12-04 23:41:16 -0600

    register auth middleware in global middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,18 @@
 import authMiddleware from 'middleware/auth'
 import {type NextRequest} from 'next/server'
 
-const middlewares = [authMiddleware]
+const middlewares: Middleware[] = [authMiddleware]
 
 export async function middleware(req: NextRequest) {
-  for (const m of middlewares) {
-    if (m.config.match && req.nextUrl.pathname.startsWith(m.config.match)) {
-      return m.handler(req)
+  middlewares.reduce(async (acc, middleware) => {
+    await acc
+
+    if (middleware.matcher(req)) {
+      return middleware.handler(req)
     }
-  }
+
+    return null
+  }, Promise.resolve())
 
   return null
 }

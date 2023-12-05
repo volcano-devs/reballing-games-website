f3f15f086b4b4aa6ec32eeced0dfa5c921943594
aguilarkevin <kevinaguilar0813@gmail.com>
2023-12-04 23:40:46 -0600

    create middleware to handle authentication

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,10 +2,10 @@ import {createMiddlewareClient} from '@supabase/auth-helpers-nextjs'
 import {type NextRequest, NextResponse} from 'next/server'
 import {Database} from 'types/database'
 
-const authMiddleware = {
-  config: {
-    match: '/dashboard',
-  },
+const authMiddleware: Middleware = {
+  matcher: (request) =>
+    request.nextUrl.pathname.match(/^\/dashboard/) ||
+    request.nextUrl.pathname === '/login',
   handler: async (req: NextRequest) => {
     const res = NextResponse.next()
     const supabase = createMiddlewareClient<Database>({req, res})
@@ -14,8 +14,16 @@ const authMiddleware = {
       data: {user},
     } = await supabase.auth.getUser()
 
+    if (user) {
+      return NextResponse.next()
+    }
+
     if (!user && req.nextUrl.pathname.match(/^\/dashboard/)) {
       return NextResponse.redirect(new URL('/login', req.nextUrl))
+    }
+
+    if (user && req.nextUrl.pathname.startsWith('/login')) {
+      return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
     }
 
     return res

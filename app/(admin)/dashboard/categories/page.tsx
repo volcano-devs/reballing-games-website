288bd311df249dd17fb702814d5d41c5cc8de033
aguilarkevin <kevinaguilar0813@gmail.com>
2024-01-07 22:40:53 -0600

    implements page categories list

--- app/(admin)/dashboard/categories/page.tsx
+++ app/(admin)/dashboard/categories/page.tsx
@@ -1,5 +1,36 @@
-export interface CategoriesPageProps {}
+import {getRange} from 'lib/pagination'
+import {createServerClient} from 'lib/supabase'
+import CategoriesList from 'sections/admin/categories/CategoriesList'
 
-export default function CategoriesPage(props: CategoriesPageProps) {
-  return <div>categories</div>
+export interface CategoriesPageProps {
+  searchParams: {
+    page?: string
+  }
+}
+
+async function getCategories({page}: {page?: number}) {
+  const supabase = createServerClient()
+
+  const range = getRange(page ?? 1)
+
+  const categories = await supabase
+    .from('categories')
+    .select('*, categories(parent_id, name)', {count: 'exact'})
+    .range(range.start, range.end)
+
+  return categories
+}
+
+export default async function CategoriesPage({
+  searchParams,
+}: CategoriesPageProps) {
+  const response = await getCategories({
+    page: searchParams.page ? parseInt(searchParams.page) : 1,
+  })
+
+  console.log(response)
+
+  return (
+    <CategoriesList categories={response.data ?? []} count={response.count} />
+  )
 }

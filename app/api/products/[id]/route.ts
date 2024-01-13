9970071a10bd04074a6edc236c78f335b8899699
aguilarkevin <kevinaguilar0813@gmail.com>
2024-01-13 11:37:23 -0600

    add some base API route handlers

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,9 @@
+export async function GET(request: Request) {}
+
+export async function POST(request: Request) {}
+
+export async function PUT(request: Request) {}
+
+export async function DELETE(request: Request) {}
+
+export async function PATCH(request: Request) {}

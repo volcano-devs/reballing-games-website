eea78d1c23f9b65cb519dae516dabbb1ad2b3a81
aguilarkevin <kevinaguilar0813@gmail.com>
2024-01-13 09:51:46 -0600

    add some types

--- types/models.d.ts
+++ types/models.d.ts
@@ -4,11 +4,30 @@ declare interface BaseModel {
   updated_at: string
 }
 
-declare interface Categories extends BaseModel {
+declare interface Product extends BaseModel {
+  name: string
+  description: string | null
+  price: number
+  thumbnail: string | null
+  category_id: number
+}
+
+declare interface Category extends BaseModel {
   description: string | null
   display_type: string
   name: string
   parent_id: string | null
   slug: string
+  thumbnail: string | null
+}
+
+declare interface Tag extends BaseModel {
+  name: string
+}
+
+declare interface Service extends BaseModel {
+  name: string
+  description: string | null
+  price: number
   thumbnail: string | null
 }

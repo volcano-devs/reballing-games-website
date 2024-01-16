e60e52e5e26d1d42e7d69c22cf2c52d8d3565463
aguilarkevin <kevinaguilar0813@gmail.com>
2024-01-15 21:58:33 -0600

    fixes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,53 +1,38 @@
 import {Button} from '@nextui-org/button'
-import {Card, CardBody} from '@nextui-org/card'
+import Image from 'next/image'
+import Link from 'next/link'
+import consolesGroup from 'public/consolas-group.png'
 
 export default function Home() {
   return (
-    <main>
-      <section className="min-h-screen flex flex-col items-center justify-center">
-        <div className="flex flex-col gap-10 w-full max-w-screen-2xl mx-auto">
-          <div className="">
-            <div className="flex flex-col gap-4">
-              <div>
-                <h1
-                  className="text-6xl
-            text-primary-500
-            "
-                >
-                  Servicio técnico especializado
-                </h1>
-                <p className="text-gray-300 text-lg font-medium">
-                  Damos soporte técnico a tus consolas y mandos de videojuegos.
-                </p>
-              </div>
-              <Button color="primary" variant="shadow" className="w-max">
-                Consultar
-              </Button>
+    <>
+      <div className="text-white bg-hero static top-0">
+        <div
+          className="container mx-auto flex w-full flex-col
+      justify-center gap-20 xl:flex-row min-h-screen lg:min-h-[1080px]"
+        >
+          <div className="flex max-w-2xl flex-col self-center px-6 text-center md:px-8 md:text-left lg:px-0">
+            <h1 className=" text-4xl font-bold  tracking-tight sm:text-6xl">
+              Servicio técnico especializado
+            </h1>
+            <p className="mt-6 text-lg leading-8">
+              Damos soporte técnico a tus consolas y mandos de videojuegos.
+            </p>
+
+            <div className="mt-10 flex w-full flex-col gap-x-4 gap-y-4 md:flex-row md:items-center">
+              <Link href="#">
+                <Button color="primary" className="w-full md:w-auto">
+                  Consultar
+                </Button>
+              </Link>
             </div>
           </div>
-
-          <div className="flex gap-4">
-            {/* <Card
-              isBlurred
-              shadow="none"
-              className="
-            bg-background-500
-            "
-            >
-              <CardBody>
-                <div className="flex flex-col">
-                  <h3 className="text-2xl font-bold">Calidad</h3>
-                  <p className="text-gray-300 text-xs font-medium">
-                    Trabajamos con altos estándares de calidad en todas nuestras
-                    reparaciones y herramientas de trabajos en manos de
-                    profesionales.
-                  </p>
-                </div>
-              </CardBody>
-            </Card> */}
+          <div className="self-center md:w-[440px] md:self-end lg:w-[540px] xl:w-[760px]">
+            <Image className="w-full" src={consolesGroup} alt="consoles" />
           </div>
         </div>
-      </section>
-    </main>
+      </div>
+      <div className="min-h-screen "></div>
+    </>
   )
 }

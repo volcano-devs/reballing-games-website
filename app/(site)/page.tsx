319d9aaf90f445684f3f424e687c6bd7b0cc0457
aguilarkevin <kevinaguilar0813@gmail.com>
2024-01-16 01:29:30 -0600

    add base services section

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,6 +2,84 @@ import {Button} from '@nextui-org/button'
 import Image from 'next/image'
 import Link from 'next/link'
 import consolesGroup from 'public/consolas-group.png'
+import chip from 'public/chip.png'
+
+const services = [
+  {
+    title: 'Rework BGA (REBALLING)',
+    description:
+      'Reballing proviene del tipo soldaduras que se usa con los componentes electrónicos, llamado Ball Grid Array (BGA). Este reboleado implica cambiar las esferas de soldadura y es solo uno de los pasos del proceso completo, el rework: Hay que fundir el estaño para eliminar las soldaduras viejas y extraer el chip.',
+  },
+  {
+    title: 'Luces Rojas (XBOX 360)',
+    description:
+      'Reparamos todos los errores de luces rojas. La luces rojas es provocada, por falta de ventilación, Cortos electrónicos o bien el chip bga , no está haciendo contacto con la placa madre.',
+  },
+  {
+    title: 'Desbloqueo Nintendo Switch',
+    description:
+      'Liberamos la consola para tener acceso de una manera que nunca fue prevista por los creadores de su sistema. Te permite instalar aplicaciones y juegos no oficiales de la Switch. Incluye Dongle R4 S',
+  },
+  {
+    title: 'Kernel Xploit PS4',
+    description: (
+      <>
+        VALIDO SEGÚN VERSION DE LA CONSOLA <br />
+        Válido para FW 5.05, 6.72, 7.20 <br /> Offline: SI <br />
+        Online: NO
+      </>
+    ),
+  },
+  {
+    title: 'Reset Glitch Hack (RGH) Xbox 360',
+    description:
+      'Se modifica el sistema original , mediante hardware. Instalando un chip, la cual nos permita emular sistema no oficiales.',
+  },
+  {
+    title: 'Desbloqueo CFW Rebug + MULTIMAN',
+    description:
+      'Sé modifica el OFW de la PS3 mediante software, parcheando la NOR, una vez parcheada se le instala CFW, para poder emular. Sistemas no oficiales .',
+  },
+  {
+    title: 'Cargas Digitales PlayStation 3',
+    description:
+      'Se instala HFW, HEN , más Juegos a tu elección. y otras aplicaciones no oficiales.',
+  },
+  {
+    title: 'Desbloqueo Nintendo wii',
+    description:
+      'Liberamos la consola para tener acceso de una manera que nunca fue prevista por los creadores de su sistema. Te permite instalar aplicaciones y juegos no oficiales de la Wii.',
+  },
+  {
+    title: 'Desbloqueo de 3DS',
+    description:
+      'Instalamos aplicaciones no oficiales para poder emular todos los juegos que existen en Nintendo 3ds.',
+  },
+  {
+    title: 'Mantenimiento Preventivo',
+    description:
+      'Damos mantenimiento a todas las consolas de juego (PS2, PS3, PS4, Xbox 360, Xbox One, Wii, Switch). INCLUYE: Cambio de pasta térmica de la mejor calidad (MX4 ARTIC SILVER), Lavado de tarjeta madre y Limpieza de case.',
+  },
+  {
+    title: 'Reparación de mandos',
+    description:
+      'Arreglamos tu mando de juego de las consolas de juego PS2, PS3, PS4, Xbox 360, Xbox One, Wii, Switch.',
+  },
+  {
+    title: 'Tarjetas de Compra',
+    description: 'Compra en las tiendas de Xbox, Playstation y Nintendo',
+  },
+  {
+    title: 'Desbloqueo PS VITA',
+    description:
+      'Instalamos aplicaciones no oficiales para poder emular juegos de ps vita o bien de psp.',
+  },
+  {
+    title: 'Chip PS2 Slim o Fat',
+    description:
+      'Instalación de chip físico o bien virtual para todas las ps2.',
+  },
+]
 
 export default function Home() {
   return (
@@ -114,9 +192,10 @@ export default function Home() {
           </div>
         </section>
       </div>
-      <section className="container mx-auto py-20">
+      {/* //products section */}
+      <section id="services" className="container mx-auto py-20">
         <div className="mt-4 md:mt-0 text-center max-w-xl mx-auto">
-          <h1 className="text-3xl font-medium text-red-600 capitalize dark:text-white">
+          <h1 className="text-2xl font-semibold tracking-tight xl:text-3xl text-red-600 capitalize dark:text-white">
             Nuestros productos
           </h1>
 
@@ -184,8 +263,78 @@ export default function Home() {
           </div>
         </div>
       </section>
-      //contact call to action
-      <section className="bg-white dark:bg-gray-900 py-20">
+      {/* //services section */}
+      <section className="container mx-auto bg-hero rounded-3xl mt-16">
+        <div className="container px-12 py-12 mx-auto">
+          <div className="lg:-mx-6 lg:flex lg:items-center justify-center gap-16">
+            <div className="mt-8 lg:w-1/3 lg:px-6 lg:mt-0">
+              <p className="mt-6 text-lg font-medium text-red-500">Servicio</p>
+
+              <h2 className="text-2xl font-semibold text-white lg:text-3xl lg:w-96">
+                {services[0].title}
+              </h2>
+
+              <p className="max-w-lg mt-6 text-gray-400 ">
+                {services[0].description}
+              </p>
+
+              <div className="flex items-center justify-between mt-12 lg:justify-start">
+                <button
+                  title="left arrow"
+                  className="p-2  transition-colors duration-300 border rounded-full rtl:-scale-x-100 border-gray-700 text-gray-200  hover:bg-gray-100 hover:text-red-500"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15 19l-7-7 7-7"
+                    />
+                  </svg>
+                </button>
+
+                <button
+                  title="right arrow"
+                  className="p-2  transition-colors duration-300 border rounded-full rtl:-scale-x-100 border-gray-700 text-gray-200  lg:mx-6 hover:bg-gray-100 hover:text-red-500"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+
+            <div className="w-56">
+              <Image
+                className="object-cover  w-full h-full"
+                src={chip.src}
+                alt="chip"
+                width={chip.width}
+                height={chip.height}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* //contact call to action */}
+      <section className="bg-white dark:bg-gray-900 py-20 mt-16">
         <div className="container flex flex-col items-center px-4 py-12 mx-auto text-center">
           <h2 className="max-w-2xl mx-auto text-2xl font-semibold tracking-tight text-gray-800 xl:text-3xl dark:text-white">
             ¿Tienes alguna duda o consulta?{' '}

fa7f2af1135dd9c44f1cd7834fd63d24ff36c802
aguilarkevin <kevinaguilar0813@gmail.com>
2024-01-15 21:48:50 -0600

    implements tag list page

--- app/(admin)/dashboard/tags/page.tsx
+++ app/(admin)/dashboard/tags/page.tsx
@@ -1,5 +1,32 @@
-export interface TagsPageProps {}
+import TagList from 'sections/admin/tags/TagList'
 
-export default function TagsPage(props: TagsPageProps) {
-  return <div>tags</div>
+export interface TagsPageProps {
+  searchParams: {
+    page?: string
+  }
+}
+
+async function getData({page}: {page?: number}) {
+  const res = await await fetch(
+    `${process.env.NEXT_URL}/api/tags?page=${page ?? 1}`,
+    {
+      headers: {
+        accept: 'application/json',
+      },
+      cache: 'no-cache',
+    },
+  )
+
+  return res
+}
+
+export default async function TagsPage({searchParams}: TagsPageProps) {
+  const response: Response & {
+    data?: Category[]
+    count?: number
+  } = await getData({
+    page: searchParams.page ? parseInt(searchParams.page) : 1,
+  }).then((res) => res.json())
+
+  return <TagList tags={response.data! ?? []} count={response.count ?? 0} />
 }

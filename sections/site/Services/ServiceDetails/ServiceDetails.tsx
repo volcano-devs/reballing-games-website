879428ffcfa0145ebf5c7307527fbd3841d272a7
aguilarkevin <kevinaguilar0813@gmail.com>
2024-10-04 00:42:16 -0600

    refactor

--- sections/site/Services/ServiceDetails/ServiceDetails.tsx
+++ sections/site/Services/ServiceDetails/ServiceDetails.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React from 'react'
 import {Button} from '@nextui-org/button'
 import {Input} from '@nextui-org/input'
 import {type BaseSyntheticEvent, useState} from 'react'

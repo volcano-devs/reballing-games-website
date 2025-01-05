f2dec1ce502152a86dbd9716f7da5b43c956a5ad
aguilarkevin <kevinaguilar0813@gmail.com>
2025-01-05 00:23:58 -0600

    feat: add react integration for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   env: {
     schema: {}
-  }
+  },
+
+  integrations: [react()]
 });

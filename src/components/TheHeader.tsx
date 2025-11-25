aeb550aa45f438a375ad7076ce1df41d8616baf5
Kevin Aguilar <kevinaguilar0813@gmail.com>
2025-11-25 00:13:18 -0600

    improve header

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,5 +1,7 @@
 import Navigation from './TheNavigation'
 import Logo from './TheLogo'
+import {Button} from './ui/button'
+import {Menu, ShoppingCart} from 'lucide-react'
 
 export default function TheHeader() {
   return (
@@ -15,41 +17,15 @@ export default function TheHeader() {
           </div>
 
           <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">
-              <a
-                className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm dark:hover:bg-teal-500"
-                href="#"
-              >
-                Login
-              </a>
-
-              <div className="hidden sm:flex">
-                <a
-                  className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Register
-                </a>
-              </div>
-            </div>
+            <Button variant="ghost">
+              $0
+              <ShoppingCart />
+            </Button>
 
             <div className="block md:hidden">
-              <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </button>
+              <Button variant="outline" size="icon">
+                <Menu />
+              </Button>
             </div>
           </div>
         </div>

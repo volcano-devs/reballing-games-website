097613c1d21d54e6b16d30f4241d1612b280a87b
Kevin Aguilar <kevinaguilar0813@gmail.com>
2025-11-28 00:29:43 -0600

    feat: base service by id page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,15 +1,43 @@
+import ServiceFormSection from '#/sections/services/ServiceForm'
+
 export interface ServicePageProps {
-  params: Promise<{
-    order_id: string
-  }>
+  searchParams: Promise<{[key: string]: string | undefined}>
+}
+
+async function getData(searchParams: SearchParams) {
+  console.log('Fetching data for order_id:', searchParams.order_id)
+
+  if (!searchParams.order_id) {
+    return {
+      data: {
+        order_id: '',
+        status: null,
+      },
+    }
+  }
+
+  const res = await fetch(
+    `${process.env.NEXT_URL}/api/services/${searchParams.order_id}`,
+    {
+      headers: {
+        accept: 'application/json',
+      },
+      cache: 'no-cache',
+    },
+  )
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
 }
 
 export default async function ServicePage(props: ServicePageProps) {
-  const {order_id} = await props.params
+  const {order_id} = await props.searchParams
+  const response = await getData({order_id})
 
-  return (
-    <div>
-      <h1>Service Order ID: {order_id ? order_id : 'No Order ID Provided'}</h1>
-    </div>
-  )
+  console.log('ServicePage fetched data:', response)
+
+  return <ServiceFormSection orderId={order_id} service={response.data} />
 }

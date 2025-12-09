e89d47cadae90eac18e4897cb0c4bdbb9ec7f813
Kevin Aguilar <kevinaguilar0813@gmail.com>
2025-12-09 01:55:11 -0600

    improve homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import {
   HeroSection,
   // TrendingProductsSection,
 } from '#/sections/home'
+import {CategoriesSection} from '#/sections/shop'
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       {/* <TrendingProductsSection /> */}
+      <CategoriesSection />
       <BrandsSection />
     </>
   )
